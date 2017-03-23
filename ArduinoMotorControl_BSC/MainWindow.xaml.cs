07d42e66751b2a55df9ea12bb2f396c5ad1a6142
Bojun Lin <bojun.lin@glidewelldental.com>
2017-03-22 17:35:08 -0700

    First commit
    First commit for my motion controller code

---
+++ ArduinoMotorControl_BSC/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using CommandMessenger.TransportLayer;
+using CommandMessenger;
+
+namespace ArduinoMotorControl_BSC
+{
+    enum Command
+    {
+        setLED,
+        rotateAngle, // Command to request led to be set in specific state
+    };
+
+    enum Motors
+    {
+        motorA,
+        motorB
+    };
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SerialTransport Arduino;
+        private CmdMessenger _cmdMessenger;
+        private bool controllerReady = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Connection()
+        {
+            // Create Serial Port object
+            Arduino = new SerialTransport();
+            Arduino.CurrentSerialSettings.PortName = Cmb_portName.Text;   // Set com port
+            Arduino.CurrentSerialSettings.BaudRate = Convert.ToInt32(Cmb_baudrate.Text);  //Int16 is from -32768 ~32767, not enough
+
+            // Initialize the command messenger with the Serial Port transport layer
+            _cmdMessenger = new CmdMessenger(Arduino);
+
+            // Tell CmdMessenger if it is communicating with a 16 or 32 bit Arduino board
+            _cmdMessenger.BoardType = BoardType.Bit16;
+
+            // Start listening
+            _cmdMessenger.StartListening();
+        }
+        
+        public void Exit()
+        {
+            // Stop listening
+            _cmdMessenger.StopListening();
+
+            // Dispose Command Messenger
+            _cmdMessenger.Dispose();
+
+            // Dispose Serial Port object
+            Arduino.Dispose();
+        }
+
+        private void Btn_connect_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Connection();
+                lbl_status.Content = "Connected!";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please give a valid port number or check your connection");
+            }
+        }
+
+        private void Btn_rotate_Click(object sender, RoutedEventArgs e)
+        {
+            if (TB_angle.Text != "")
+                Rotate((int)Motors.motorA, Convert.ToDouble(TB_angle.Text));
+        }
+
+        private void Rotate(int motorIndex, double angle)
+        {
+            // Create command
+            var command = new SendCommand((int)Command.rotateAngle);
+            // Send command
+            command.AddArgument(motorIndex);
+            command.AddArgument(angle);
+            _cmdMessenger.SendCommand(command);
+        }
+
+        public void ToggleLED(int index, bool signal)
+        {
+            // Create command
+            var command = new SendCommand((int)Command.setLED);
+            // Send command
+            command.AddArgument(index);
+            command.AddArgument(signal);
+
+            _cmdMessenger.SendCommand(command);
+
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ToggleLED(13, true);
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ToggleLED(13, false);
+        }
+    }
+}
---
+++ MotorControl_BSC/MotorControl_BSC.ino
@@ -0,0 +1,112 @@
+#include <CmdMessenger.h>  // CmdMessenger
+
+const int motorADirPin = 8;
+const int motorAStepPin = 9;
+
+const int motorBDirPin = 10;
+const int motorBStepPin = 11;
+
+const int ledPin = 13;
+
+int numberOfSteps = 200;
+int numberOfMicroSteps = 2000;
+int pulseWidth = 5;  // microseconds 1~60
+int stepInterval = 100; // microseconds
+
+// Attach a new CmdMessenger object to the default Serial port
+CmdMessenger cmdMessenger = CmdMessenger(Serial);
+
+// Commands
+enum
+{
+  setLED,
+  rotateAngle, // 
+};
+
+// Callbacks define on which received commands we take action 
+void attachCommandCallbacks()
+{
+  cmdMessenger.attach(rotateAngle, OnRotateAngle);
+  cmdMessenger.attach(setLED, OnSetLed);
+}
+
+void setup() { 
+  pinMode(motorADirPin, OUTPUT);
+  pinMode(motorAStepPin, OUTPUT);
+  pinMode(motorBDirPin, OUTPUT);
+  pinMode(motorBStepPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  
+  digitalWrite(motorADirPin, LOW);
+  digitalWrite(motorAStepPin, LOW);
+  digitalWrite(motorBDirPin, LOW);
+  digitalWrite(motorBStepPin, LOW);
+
+  Serial.begin(115200);  
+  // Adds newline to every command
+  cmdMessenger.printLfCr();   
+  // Attach my application's user-defined callback methods
+  attachCommandCallbacks();
+  //Serial.write("GO?");
+}
+
+int motorIndex;
+double angle;
+
+// Function code
+void OnRotateAngle(){
+  motorIndex = cmdMessenger.readInt16Arg();
+  angle = cmdMessenger.readDoubleArg();
+  
+  switch (motorIndex)
+  {
+    case 0:
+        _runAngle(motorADirPin, motorAStepPin, angle);break;
+    case 1:
+        _runAngle(motorBDirPin, motorBStepPin, angle);break;
+  }
+}
+
+// Motor Interaction code
+void _runAngle(int directionPin, int stepPin, double angle){
+    if (angle < 0)
+    {
+      angle = abs(angle);
+      //digitalWrite(ledPin, LOW);
+      digitalWrite(directionPin, LOW);
+    }
+    else
+    {
+      //digitalWrite(ledPin, HIGH);
+      digitalWrite(directionPin, HIGH);
+    }
+    
+    for (int i = 0; i < (numberOfMicroSteps * angle) / 360.0; i ++)
+    {    
+      delayMicroseconds(pulseWidth);
+      digitalWrite(stepPin, HIGH);
+      delayMicroseconds(pulseWidth);
+      digitalWrite(stepPin, LOW);
+      delayMicroseconds(stepInterval);
+    }
+  
+}
+
+// Blinking led variables 
+bool ledState = 0;   // Current state of Led
+int ledIndex = 0;
+
+void OnSetLed()
+{
+  ledIndex = cmdMessenger.readInt16Arg();
+  ledState = cmdMessenger.readBoolArg();
+  switch (ledIndex)
+  {
+    case 13: digitalWrite(ledPin, ledState?HIGH:LOW);break;
+  }
+}
+
+void loop() { 
+  // Process incoming serial data, and perform callbacks
+  cmdMessenger.feedinSerialData();
+}
